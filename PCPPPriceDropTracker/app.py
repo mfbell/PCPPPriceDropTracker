554ec43b243e81ef7e1b3d8212adefa8fa786bc3
mtech <kind.of.random@outlook.com>
2017-06-02 00:09:11 +0100

    Updated to new logging system

--- PCPPPriceDropTracker/app.py
+++ PCPPPriceDropTracker/app.py
@@ -19,6 +19,7 @@ __doc__ = __doc__.format(AUTHOR, VERSION, STATUS, LICENSE, URL)
 
 import tkinter as tk
 import tkinter.ttk as ttk
+from logging import getLogger as gl
 from .DBHandler import Handler
 from .tools import Tools
 from .customWidgets import Panel, MessageBox
@@ -36,6 +37,8 @@ class App(Panel):
         db_handler - Database handler object | object
 
         """
+        logger = gl(__name__+".App.__init__")
+        logger.debug("App initalization.")
         if "db_handler" in kwargs:
             self.db_handler = kwargs["db_handler"]
             del(kwargs["db_handler"])
@@ -44,10 +47,9 @@ class App(Panel):
         if not "padding" in kwargs:
             kwargs["padding"] = (12, 6, 6, 6)
         super().__init__(root, *args, **kwargs)
-        #self.debug(True)
         self.pack()
         if not self.db_handler:
-            self.db_handler = Handler(debug=self.debug)
+            self.db_handler = Handler()
 
         # Window setup
         self.root.title("PCPPPriceDropTracker {0}".format(VERSION))
@@ -60,14 +62,14 @@ class App(Panel):
                              "Active": ["Active", 100]
                              }
         # Content
-        self.title_zone = Title_Panel(self, debug=self.debug)
-        self.search_filters = Search_Filter_Panel(self, debug=self.debug)
+        self.title_zone = Title_Panel(self)
+        self.search_filters = Search_Filter_Panel(self)
         self.right_bar = ttk.Separator(self, orient="vertical")
-        self.search_box = Search_Box(self, debug=self.debug)
-        self.results_panel = Results_Panel(self, borderwidth=2, relief="sunken", debug=self.debug)
-        self.status_bar = Status_Bar(self, debug=self.debug)
+        self.search_box = Search_Box(self)
+        self.results_panel = Results_Panel(self, borderwidth=2, relief="sunken")
+        self.status_bar = Status_Bar(self)
         self.menu_bar = Menu_Bar(self)
-        self.side_options = Side_Options(self, debug=self.debug)
+        self.side_options = Side_Options(self)
         #self.left_bar = ttk.Separator(self, orient="vertical")
         # Packing
         self.title_zone.grid(column=8, row=0)
@@ -78,6 +80,8 @@ class App(Panel):
         self.search_filters.grid(column=6, row=2, sticky="n", padx=(0,6), pady=(3))
         self.status_bar.grid(column=0, row=99, columnspan=32, sticky="s")
         #self.left_bar.grid(column=7, row=2, sticky="ns", padx=(6), pady=(3))
+        logger.debug("App setup complete.")
+        logger.info("PCPPPriceDropTracker GUI is running")
         return None
 
     def add_filter(self):
@@ -94,10 +98,13 @@ class Title_Panel(Panel):
 
     def __init__(self, root, *args, **kwargs):
         """Initialization. Args as of Panel."""
+        logger = gl(__name__+".Title_Panel.__init__")
+        logger.debug("Title_Panel initalization.")
         super().__init__(root, *args, **kwargs)
         self.text = ttk.Label(self, text="PCPPScraper\nWriten by mtech0")
         # Packing
         self.text.grid(row=0)
+        logger.debug("Title_Panel setup complete.")
         return None
 
 
@@ -106,6 +113,8 @@ class Search_Box(Panel):
 
     def __init__(self, root, *args, **kwargs):
         """Initialization. Args as of Panel."""
+        logger = gl(__name__+".Search_Box.__init__")
+        logger.debug("Search_Box initalization.")
         super().__init__(root, *args, **kwargs)
         self.search_text = tk.StringVar()
         self.search_text.trace("w", self.search)
@@ -114,27 +123,32 @@ class Search_Box(Panel):
         # Packing
         self.search_box.grid(column=0, row=0)
         self.search_button.grid(column=1, row=0)
+        logger.debug("Search_Box setup complete.")
         return None
 
     def search(self, *args, **kwargs):
         """Search for offer in database and display results."""
-        self.debug_msg("Search in Search_Box called")
+        logger = gl(__name__+".Search_Box.search")
         string = self.search_text.get()
-        self.debug_msg("Search string is: "+string)
+        logger.debug("All to search with string: '%s'.", string)
         if not string or string == "":
-            self.debug_msg("Nothing in string, showing all with filters")
+            logger.debug("No value in string, showing all with filters applied.")
             return self.root.results_panel.show_all_w_filters()
         results = self.root.db_handler.search(self.root.show_columns, self.root.search_filters.get(), string)
         if not results:
+            logger.debug("No result from search query.")
             return None
         elif "external" in kwargs and kwargs["external"]:
-            Results_Panel(tk.Tk(), debug=self.debug, open_search_data=[self.root.show_columns, self.search_text.get(), results])
+            logger.debug("Search results been opened external.")
+            Results_Panel(tk.Tk(), open_search_data=[self.root.show_columns, self.search_text.get(), results])
             return None
         self.root.results_panel.clear()
+        logger.debug("Search results sent to be added to results panel.")
         return self.root.results_panel.add(results)
 
     def external_open(self):
         """Search external open rapper method."""
+        gl(__name__+".Search_Box.external_open").debug("External open called.")
         return self.search(external=True)
 
 
@@ -150,12 +164,15 @@ class Results_Panel(Panel):
             / root must be tk.Tk() for this as it also sets a title to root.
 
         """
+        logger = gl(__name__+".Results_Panel.__init__")
+        logger.debug("Results_Panel initalization.")
         data = None
         if "open_search_data" in kwargs:
+            logger.debug("Results panel to open externally.")
             self.columns, self.search_text, data = kwargs["open_search_data"]
             del(kwargs["open_search_data"])
         super().__init__(root, *args, **kwargs)
-        if not data:
+        if data is None:
             self.columns = self.root.show_columns
         self.tree = ttk.Treeview(self, column=([col for col in self.columns][1:]), height=25)
         first = True
@@ -179,14 +196,17 @@ class Results_Panel(Panel):
             self.add(data)
         else:
             self.show_all_w_filters() # Remember this is done on first creation.
+        logger.debug("Results_Panel setup complete.")
         return None
 
     def clear(self):
         """Clear the Treeview/results."""
+        gl(__name__+".Results_Panel.clear").debug("Clear panel")
         return self.tree.delete(*self.tree.get_children())
 
     def show_all_w_filters(self):
         """Show all <conditions -only active=1 at the moment> offers."""
+        gl(__name__+".Results_Panel.show_all_w_filters").debug("Showing all entries with filters applied to results panel.")
         self.clear()
         results = self.root.db_handler.search(self.root.show_columns, self.root.search_filters.get(), None)
         return self.add(results)
@@ -197,6 +217,7 @@ class Results_Panel(Panel):
         ids - A list/tuple of OfferIDs | list/tuple
             / Can be [(id,), ...] as got from query.
         """
+        gl(__name__+".Results_Panel.add_by_id").debug("Adding entries by id.")
         if isinstance(ids[0], tuple):
             wasIds = ids
             ids = [id[0] for id in wasIds]
@@ -208,6 +229,7 @@ class Results_Panel(Panel):
 
         data - Results to add | [[col1, col2, ...], ...] or tuplised
         """
+        gl(__name__+".Results_Panel.add").debug("Adding results to Treeview")
         for item in data:
             self.tree.insert('', "end", text=item[0], values=(item[1:]))
         return None
@@ -218,6 +240,8 @@ class Side_Options(Panel):
 
     def __init__(self, root, *args, **kwargs):
         """Initialization. Args as of Panel."""
+        logger = gl(__name__+".Side_Options.__init__")
+        logger.debug("Side_Options initialization.")
         super().__init__(root, *args, **kwargs)
 
         self.update = ttk.Button(self, text="Update", command=lambda: Update_Hanlder(self.root))
@@ -234,7 +258,6 @@ class Side_Options(Panel):
         self.exit = ttk.Button(self, text="Exit", command=self.root.quit)
 
         self.set_textvars()
-
         # Packing
         self.update.grid(row=0, pady=(3,0))
         self.show_all.grid(row=1, pady=(3,0))
@@ -246,34 +269,32 @@ class Side_Options(Panel):
 
         self.clear.grid(row=98, pady=(3,0))
         self.exit.grid(row=99, pady=(3,0), sticky="s")
+        logger.debug("Side_Options setup complete.")
         return None
 
     def debug_change(self):
         """Change debug status and update GUI."""
-        if self.debug():
-            self.debug(False)
-        else:
-            self.debug(True)
-        return self.set_textvars()
+        logger = gl(__name__+".Side_Options.debug_change")
+        logger.debug("Debug change called.")
+        logger.warning("Debug setting changing is not currently supported with the new logging system. This is to be add in a future update.")
+        return None
 
     def log_change(self):
         """Change log status and update GUI."""
-        if self.debug(log="get"):
-            self.debug(log=False)
-        else:
-            self.debug(log=True)
-        return self.set_textvars()
+        logger = gl(__name__+".Side_Options.log_change")
+        logger.debug("Log change called.")
+        logger.warning("Log setting changing is not currently supported with the new logging system. This is to be add in a future update.")
+        return None
 
     def set_textvars(self):
         """Update GUI to match debug and log status."""
-        if self.debug():
-            self.debug_status.set("Debugging")
-        else:
-            self.debug_status.set("Debug")
-        if self.debug(log="get"):
-            self.log_status.set("Logging")
-        else:
-            self.log_status.set("Log")
+        logger = gl(__name__+".Side_Options.set_textvars")
+        logger.debug("Set textvariable called")
+        logger.warning("Debug setting changing is not currently supported with the new logging system. This is to be add in a future update.")
+        self.debug_button.state(['disabled'])
+        self.log_button.state(['disabled'])
+        self.debug_status.set("Debug")
+        self.log_status.set("Log")
         return None
 
 
@@ -282,23 +303,31 @@ class Search_Filter_Panel(Panel):
 
     def __init__(self, root, *args, **kwargs):
         """Initialization. Args as of Panel."""
+        logger = gl(__name__+".Search_Filter_Panel.__init__")
+        logger.debug("Search_Filter_Panel initialization.")
         super().__init__(root, *args, **kwargs)
 
         self.text = ttk.Label(self, text="Search Filters\nwill go here")
         # Packing
         self.text.grid(column=0, row=0)
+        logger.debug("Search_Filter_Panel setup complete.")
         return None
 
     def get(self):
         """Get a formated filter string."""
+        logger = gl(__name__+".Search_Filter_Panel.get")
+        logger.debug("Search_Filter_Panel.get called, returning filters")
+        logger.warning("Filters not supported yet, only Active = 1. To be added in future update.")
         return "Active = 1"
 
 
 class Status_Bar(Panel):
-    """Bottom info bar. Needs to be rewriten into own frame."""
+    """Bottom debug bar. Needs to be rewriten into own frame."""
 
     def __init__(self, root, *args, **kwargs):
         """Initialization. Args as of Panel."""
+        logger = gl(__name__+".Status_Bar.__init__")
+        logger.debug("Status_Bar initialization.")
         super().__init__(root, *args, **kwargs)
 
         self.top_line = ttk.Separator(self.root, orient="horizontal")
@@ -314,6 +343,7 @@ class Status_Bar(Panel):
         self.results_length.grid(column=8, row=96, sticky="w")
         self.bar1.grid(column=9, row=96, sticky="ns")
         self.country.grid(column=10, row=96, sticky="e")
+        logger.debug("Status_Bar setup complete.")
         return None
 
 
@@ -322,6 +352,8 @@ class Menu_Bar(Tools):
 
     def __init__(self, root, *args, **kwargs):
         """Initialization. Args as of Tools."""
+        logger = gl(__name__+".Menu_Bar.__init__")
+        logger.debug("Menu_Bar initialization.")
         self.root = root
         Tools.__init__(self, *args, **kwargs)
         self.menu = tk.Menu(self.root.root, *args, **kwargs)
@@ -340,6 +372,7 @@ class Menu_Bar(Tools):
         self.menu.add_cascade(menu=self.file_menu, label='File')
         self.menu.add_cascade(menu=self.edit_menu, label='Edit')
         self.menu.add_command(label="Update", command=lambda: Update_Hanlder(self.root))
+        logger.debug("Menu_Bar setup complete.")
         return None
 
 
@@ -348,12 +381,17 @@ class Update_Hanlder(Tools):
 
     def __init__(self, root):
         """Initialization."""
+        logger = gl(__name__+".Update_Hanlder.__init__")
+        logger.debug("Update_Hanlder initialization.")
         self.root = root
         self.create_popup()
         self.root.db_handler.updater(self.popup_updater)
+        logger.debug("Update_Hanlder setup complete")
         return None
 
     def create_popup(self):
+        logger = gl(__name__+".Update_Hanlder.create_popup")
+        logger.debug("Creating popup.")
         self.text = tk.StringVar()
         self.button = tk.StringVar()
         self.text.set("Updating local database...")
@@ -361,17 +399,23 @@ class Update_Hanlder(Tools):
         self.msg_box = MessageBox(msg=self.text,
                                   buttons={"1":[self.button, "WITHDRAW"]},
                                   title="Updating",
-                                  icon=".\imgs\info_icon.png")
+                                  icon=".\imgs\debug_icon.png")
+        logger.debug("Popup created.")
         return None
 
     def popup_updater(self):
+        logger = gl(__name__+".Update_Hanlder.popup_updater")
+        logger.debug("popup_updater called")
         self.text.set("Updated local database")
         self.button.set("Close")
         self.msg_box.buttons["1"]["command"] = self.update_and_close
         self.msg_box.deiconify()
+        self.msg_box.grab_set()
+        logger.debug("Popup updated")
         return None
 
     def update_and_close(self):
+        gl(__name__+".Update_Hanlder.update_and_close").debug("Close and update pressed.")
         self.msg_box.destroy()
         self.root.search_box.search()
         return None
@@ -382,10 +426,19 @@ def mp():
     return print("A menu button was pressed")
 
 def main():
-    """"""
+    """Main."""
+    logger = gl(__name__+".main")
+    logger.debug("Main called, calling and running App")
     app = App()
     app.mainloop()
+    logger.debug("App closed.")
     return None
 
-if __name__ == '__main__':
+def run_as_main():
+    import log_setup
+    log_setup.setup()
+    gl(__name__).debug("Run as main.")
     main()
+
+if __name__ == '__main__':
+    run_as_main()
