a96426a7991cb32a34f127303f603a1ad5ed9eb8
mtech <kind.of.random@outlook.com>
2017-06-03 23:29:21 +0100

    Added info to the top of log files

--- PCPPPriceDropTracker/log_setup.py
+++ PCPPPriceDropTracker/log_setup.py
@@ -21,31 +21,41 @@ import logging
 import logging.config
 from time import strftime
 import os
-from .tools import main
+from .tools import main, get_git_commit_hash
 
+DATETIME = strftime("%Y-%m-%d %H-%M-%S")
 LOG_DIR = ".\logs"
-logging_config = {
+LOG_FILE_FIRST_LINE = """PCPPPriceDropTracker Log File
+Commit Version: {0} ({1})
+//Should add a sha hash of files so logs can be IDed when in development//
+//Credits here//
+
+""".format(get_git_commit_hash("short"), get_git_commit_hash())
+LOG_CONFIG = {
     "version": 1,
     "disable_existing_loggers": False,
     "loggers": {
         "PCPPPriceDropTracker": {
-            "level": "DEBUG",
+            "level": "INFO",
             "handlers": ["consoleHandler", "fileHandler"],
             "propagate": False
+        },
+        "PCPPPriceDropTracker.customWidgets": {
+            "level": "DEBUG",
+            "propagate": True
         }
     },
     "handlers": {
         "consoleHandler": {
-            "level": "ERROR",
+            "level": "DEBUG",
             "formatter": "console",
             "class": "logging.StreamHandler",
-            #"args": (sys.stdout, )
         },
         "fileHandler": {
             "level": "DEBUG",
             "formatter": "file",
             "class": "logging.FileHandler",
-            "filename": LOG_DIR + strftime("\%Y-%m-%d %H-%M-%S.log"),
+            "filename": "{0}\{1}.log".format(LOG_DIR, DATETIME)
         }
     },
     "formatters": {
@@ -55,7 +65,7 @@ logging_config = {
             "style": "{"
         },
         "console": {
-            "format": "{asctime} | {levelname:8s} | line {lineno:<4} in {name:60s} - {message}",
+            "format": "{asctime} | {levelname:8s} | line {lineno:<4} in {name:60s} \n{message}\n",
             "datefmt": "%H:%M:%S",
             "style": "{"
         }
@@ -66,8 +76,13 @@ def setup():
     """Load logging config."""
     if not os.path.exists(LOG_DIR):
         os.makedirs(LOG_DIR)
-    logging.config.dictConfig(logging_config)
-    return None
+    logging.config.dictConfig(LOG_CONFIG)
+    # Only covers handlers using logging.FileHandler
+    for handler in LOG_CONFIG["handlers"]:
+        if LOG_CONFIG["handlers"][handler]["class"] == "logging.FileHandler":
+            with open(LOG_CONFIG["handlers"][handler]["filename"], "a") as f:
+                f.write(LOG_FILE_FIRST_LINE)
+    return
 
 if __name__ == "__main__":
     main(__doc__)
